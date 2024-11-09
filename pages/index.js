0eed1b1f81fc0711f7c492c277c30f25c914cab1
Robson Feijo <robsonfeijoxyz@gmail.com>
2024-11-09 19:04:02 -0300

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+function Home() {
+  return <h1>"“Porque Deus amou tanto o mundo que deu seu Filho único, para que 
+    todo o que nele crer não pereça, mas tenha a vida eterna." João 3:16</h1>
+}
+
+export default Home;
