d543e5f1f22e037d8552b7a2526b15019ab09d53
Robson Feijo <robsonfeijoxyz@gmail.com>
2024-11-10 16:20:54 -0300

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
   return<h1>
-    "Porque Deus amou tanto o mundo que deu seu Filho único, para que todo o que nele crer não pereça, mas tenha a vida eterna." 🙏
-    <br>João 3:16</br>
+    "Porque Deus amou tanto o mundo que deu seu Filho único, para que todo o que
+    nele crer não pereça, mas tenha a vida eterna." 🙏 João 3:16 ✝️
     </h1>
 }
 
